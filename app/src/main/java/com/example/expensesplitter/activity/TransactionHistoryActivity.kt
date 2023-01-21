1726018e767eab9e72ec2e705e27de3830ebea6a
AgPriyanshu18 <agpriyanshu18@gmail.com>
2023-01-21 21:44:20 +0530

    Transaction Activity Bug Fix

--- app/src/main/java/com/example/expensesplitter/activity/TransactionHistoryActivity.kt
+++ app/src/main/java/com/example/expensesplitter/activity/TransactionHistoryActivity.kt
@@ -2,14 +2,13 @@ package com.example.expensesplitter.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.expensesplitter.R
 import com.example.expensesplitter.databinding.ActivityTransactionHistoryBinding
 
 class TransactionHistoryActivity : AppCompatActivity() {
-    var binding : ActivityTransactionHistoryBinding ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityTransactionHistoryBinding.inflate(layoutInflater)
-        setContentView(binding?.root)
+        setContentView(R.layout.activity_transaction_history)
 
 
     }

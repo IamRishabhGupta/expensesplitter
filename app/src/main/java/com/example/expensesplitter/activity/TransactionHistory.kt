405d93815138030d91848779ed231248f42bb8bd
AgPriyanshu18 <agpriyanshu18@gmail.com>
2023-01-21 18:55:58 +0530

    setUp

---
+++ app/src/main/java/com/example/expensesplitter/activity/TransactionHistory.kt
@@ -0,0 +1,16 @@
+package com.example.expensesplitter.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.expensesplitter.databinding.ActivityTransactionHistoryBinding
+
+class TransactionHistory : AppCompatActivity() {
+    var binding : ActivityTransactionHistoryBinding ?= null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTransactionHistoryBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+
+    }
+}
